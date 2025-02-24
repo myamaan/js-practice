ef0b3459af3d852ffc6e3b260d7867850bbe16b3
myamaan <yamaan.hanif777@gmail.com>
2025-02-24 22:59:18 +0500

    practice pause

---
+++ app.js
@@ -0,0 +1,20 @@
+// chapter 1 Alert
+
+alert('Hello java script i am alert');
+
+// chapter 2 Variable For Strings
+
+var username = prompt ('enter your username');
+console.log(username);
+
+var myname = prompt ('enter your name');
+console.log( myname);
+
+var message = prompt ('type message');
+console.log(message);
+
+var email = prompt ('enter your email : for example xyz@gmail.com');
+console.log(email);
+
+var book  = prompt ('type book and see magic');
+console.log ( book = 'I am trying to learn from a book smarter way to learn javascript');
