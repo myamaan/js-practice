0775d8df60569386342e87061b58f0aec3713b3e
myamaan <yamaan.hanif777@gmail.com>
2025-02-28 02:50:57 +0500

    chapter 5 started

--- app.js
+++ app.js
@@ -1,20 +1,84 @@
 // chapter 1 Alert
 
-alert('Hello java script i am alert');
+// alert('Hello java script i am alert');
 
 // chapter 2 Variable For Strings
 
-var username = prompt ('enter your username');
-console.log(username);
+// var username;
+// var myName = 'Full name';
+// var message = 'hello world';
+// alert(message);
+// var name = 'yamaan';
+// alert(name);
+// var age = '18';
+// alert(age);
+// var skill = 'certified web developer';
+// alert(skill);
+// var food = (" pizza \n pizz \n piz \n pi \n p");
+// alert(food);
+// var email = ('my email address is ' + 'example@example.com');
+// alert(email);
+// var book = ('A smarter way to learn JavaScript');
+// alert(book);
+// var content = ('Yah! I can write HTML content through the JavaScript');
+// document.write(content);
+// var symbol = ('▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬');
+// alert(symbol)
 
-var myname = prompt ('enter your name');
-console.log( myname);
+// chapter 3 VARIABLES FOR NUMBERS
 
-var message = prompt ('type message');
-console.log(message);
+// var age = 'Hello i am 18 years old';
+// alert(age);
+// var birthYear = prompt ('enter your birth year');
+// document.write('My birth year is ' + birthYear +'\n data type of my declared variable is number');
+// var name = prompt ('enter your name');
+// var product = prompt ('enter product name');
+// var quantity =prompt ('enter product quantity');
+// document.write(name + ' ordered ' + quantity  + ' ' + product + ' on XYZ Clothing store');
+ 
+// chapter 4 VARIABLE NAMES: LEGAL & ILLEGAL
 
-var email = prompt ('enter your email : for example xyz@gmail.com');
-console.log(email);
+// var num1 = 10 , num2 = 20 , num3 = 30;
+// document.write(num1 + num2 + + num3)
 
-var book  = prompt ('type book and see magic');
-console.log ( book = 'I am trying to learn from a book smarter way to learn javascript');
+// 5 legal variables
+
+// var name = ('yamaan');
+// var username_ = ('myamaan');
+// var email = ('yamaan.official777@gmail.com');
+// var name$ = ('yamaan');
+// var name1 = ('yamaan');
+
+// 5 illegal variables 
+
+// var name% = ('yamaan');
+// var name-1 = ('yamaan');
+// var user name = ('yamaan');
+// var 1name ('yamaan');
+// var user+name = ('yamaan');
+
+// var heading = ('<h1>Rules for naming JS variables</h1>');
+// document.write(heading);
+// var names = ('<p> Variable names can only contain numbers letters $dollar sign and _underscore</p>');
+// document.write(names);
+// var varName = ('<p> Variables must begin with a letter. $ or _. For example $name, _name or name</p> ');
+// document.write(varName);
+// var sensitive = ('<p> Variable names are case sensitive</p>')
+// document.write(sensitive);
+// var keywords = ('<p> Variable names should not be JS keywords</p>')
+// document.write(keywords);
+
+// MATH EXPRESSIONS 
+
+
+// var myname = prompt ('enter your name');
+// console.log( myname);
+
+// var message = prompt ('type message');
+// console.log(message);
+
+// var email = prompt ('enter your email : for example xyz@gmail.com');
+// console.log(email);
+
+// var book  = prompt ('type book and see magic');
+// console.log ( book = 'I am trying to learn from a book smarter way to learn javascript');
