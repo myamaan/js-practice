c87791259070172d657bdf313b71be27a9da9e0b
myamaan <yamaan.hanif777@gmail.com>
2025-02-28 02:51:23 +0500

    title added

--- app.js
+++ app.js
@@ -70,15 +70,3 @@
 
 // MATH EXPRESSIONS 
 
-
-// var myname = prompt ('enter your name');
-// console.log( myname);
-
-// var message = prompt ('type message');
-// console.log(message);
-
-// var email = prompt ('enter your email : for example xyz@gmail.com');
-// console.log(email);
-
-// var book  = prompt ('type book and see magic');
-// console.log ( book = 'I am trying to learn from a book smarter way to learn javascript');
