b8a7dc704059b0a8dd9baad19bfccad05d9f4877
myamaan <yamaan.hanif777@gmail.com>
2025-02-28 05:59:21 +0500

    forth expression completed

--- app.js
+++ app.js
@@ -110,16 +110,24 @@
 // var cost = quantity*600;
 // document.write('The cost of buy '+ quantity + ' tickets to a movie is '+ cost+ ' PKR ');
 
-var tableHead = prompt ('enter table heading');
-document.write('<h1>Table of ' + tableHead  +'<br>'+'<br>');
-var table = prompt ('enter table value ' );
-document.write(table + ' X 1 = ' +  table * 1 +'<br>');
-document.write(table + ' X 2 = ' +  table * 2 +'<br>');
-document.write(table + ' X 3 = ' +  table * 3 +'<br>');
-document.write(table + ' X 4 = ' +  table * 4 +'<br>');
-document.write(table + ' X 5 = ' +  table * 5 +'<br>');
-document.write(table + ' X 6 = ' +  table * 6 +'<br>');
-document.write(table + ' X 7 = ' +  table * 7 +'<br>');
-document.write(table + ' X 8 = ' +  table * 8 +'<br>');
-document.write(table + ' X 9 = ' +  table * 9 +'<br>');
-document.write(table + ' X 10 = ' +  table * 10 +'<br>');
+// var tableHead = prompt ('enter table heading');
+// document.write('<h1>Table of ' + tableHead  +'<br>'+'<br>');
+// var table = prompt ('enter table value ' );
+// document.write(table + ' X 1 = ' +  table * 1 +'<br>');
+// document.write(table + ' X 2 = ' +  table * 2 +'<br>');
+// document.write(table + ' X 3 = ' +  table * 3 +'<br>');
+// document.write(table + ' X 4 = ' +  table * 4 +'<br>');
+// document.write(table + ' X 5 = ' +  table * 5 +'<br>');
+// document.write(table + ' X 6 = ' +  table * 6 +'<br>');
+// document.write(table + ' X 7 = ' +  table * 7 +'<br>');
+// document.write(table + ' X 8 = ' +  table * 8 +'<br>');
+// document.write(table + ' X 9 = ' +  table * 9 +'<br>');
+// document.write(table + ' X 10 = ' +  table * 10 +'<br>');
+
+// var celsius = prompt ('enter celsius to convert farhenaite');
+// var farhenaite = (celsius*9/5)+32 ;
+// document.write(celsius + 'oC' + ' is '+ farhenaite + 'oF');
+
+// var farhenaite = +prompt ('enter farhenite to convert celsius');
+// var celsius = (farhenaite-32)*5/9;
+// document.write(farhenaite + ' oF is ' + celsius + 'oC');
